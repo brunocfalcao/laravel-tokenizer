7324e02243cc2c257371bea0c92d9710eae80b0d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-12-23 18:58:16 +0100

    Initial commit

---
+++ src/TokenizerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brunocfalcao\Defaultables;
+
+use Illuminate\Support\ServiceProvider;
+
+class DefaultablesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
